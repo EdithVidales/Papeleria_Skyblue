f66f4703a46783593f58f03f2e068913c871e597
Edith Vidales <edithvdsvqz@gmail.com>
2022-11-13 10:47:47 -0600

    Se modificó archivo cn.php para el nuevo enlace de la base de datos.

--- cn.php
+++ cn.php
@@ -1,5 +1,5 @@
 <?php
-$conexion = mysqli_connect("localhost", "root", "268044", "evidencia3");
+$conexion = mysqli_connect("localhost", "id19844930_admin", "M3f=bAo1_?ivQeBD", "id19844930_papeleriaskyblue");
 mysqli_set_charset($conexion, "utf8");
 
 ?>
